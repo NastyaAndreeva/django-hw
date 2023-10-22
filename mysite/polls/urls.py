17cffe3e6c4b40c6a08fff5a49231615914b2a7c
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2023-10-22 21:56:17 +0300

    added forms and views hw

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.note_list, name="notes"),
+    path('note/<int:note_id>/', views.note_detail, name='note_detail'),
+    path('note/new/', views.create_or_edit_note, name='create_note'),
+    path('note/edit/<int:note_id>/', views.create_or_edit_note, name='edit_note'),
+    path('note/delete/<int:note_id>/', views.delete_note, name='delete_note'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import HttpResponse, render
 from .models import Note
+from .forms import NoteForm
+
+def create_or_edit_note(request, note_id=None):
+    note = get_object_or_404(Note, id=note_id) if note_id else None
+
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm(instance=note)
+
+    return render(request, 'note_form.html', {'form': form, 'note': note})
 
 def note_list(request):
     notes = Note.objects.all()
     return render(request, 'note_list.html', {'notes': notes})
+
+def delete_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    note.delete()
+    return redirect('note_list')
 
 def index(request):
     return HttpResponse("Hello from Notes app")
